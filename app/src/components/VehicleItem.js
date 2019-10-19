fa1a13b7deeb3ba19029d71047334340ab982093
Guido Santillan <guidosantillan01@gmail.com>
2019-10-19 13:56:01 +0200

    Create VehicleItem component

---
+++ app/src/components/VehicleItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VehicleItem = () => {
+  return (
+    <div>
+      <h3>Vehicle</h3>
+    </div>
+  );
+};
+
+export default VehicleItem;
--- app/src/components/VehiclesList.js
+++ app/src/components/VehiclesList.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import VehicleItem from './VehicleItem';
+
 const VehiclesList = ({ vehicles }) => {
   return (
     <div>
       <h2>List of vehicles</h2>
+      <VehicleItem />
+      <VehicleItem />
+      <VehicleItem />
+      <VehicleItem />
     </div>
   );
 };
