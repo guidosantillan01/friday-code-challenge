06f437ac29675576ffe1244c8f6c8eecba951659
Guido Santillan <guidosantillan01@gmail.com>
2019-10-19 13:42:03 +0200

    Create VehiclesList component

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import VehiclesList from './VehiclesList';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>FRIDAY Car Selector App</h1>
+      <VehiclesList />
     </div>
   );
 }
